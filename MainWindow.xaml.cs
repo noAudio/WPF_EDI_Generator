d94d8a77a9cd11c427e57f527848848f995242d2
Samuel Odundo <samieodd@gmail.com>
2020-02-24 01:25:38 +0300

    Added select ouput path feature

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,11 +9,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace DrLitovs837pGenerator
 {
@@ -24,26 +26,34 @@ namespace DrLitovs837pGenerator
     {
         string selectedFileFormat;
         string recordsFilePath;
+        string outputPath;
 
         public MainWindow()
         {
             InitializeComponent();
+            gridInputSelection.Visibility = Visibility.Visible;
         }
 
         private void radioButton_Copy_Checked(object sender, RoutedEventArgs e)
         {
-            selectedFileFormat = "Google Sheets";
+            selectedFileFormat = "Remote";
+            gridInputSelection.Visibility = Visibility.Hidden;
         }
 
         private void radioButton_Copy1_Checked(object sender, RoutedEventArgs e)
         {
-            selectedFileFormat = "Local Excel";
+            selectedFileFormat = "Local";
+
+            //if (gridInputSelection.Visibility == Visibility.Hidden)
+            //{
+            //    gridInputSelection.Visibility = Visibility.Visible;
+            //}
         }
 
         private void buttonSelectLocalFile_Click(object sender, RoutedEventArgs e)
         {
             // create an object to handle grabbing a file
-            OpenFileDialog localFileLocation = new OpenFileDialog();
+            Microsoft.Win32.OpenFileDialog localFileLocation = new Microsoft.Win32.OpenFileDialog();
             // add filter for excel files
             localFileLocation.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
             // check if the windows file selector is launched
@@ -51,9 +61,22 @@ namespace DrLitovs837pGenerator
             {
                 // pass the path of the file to the recordsFilePath string
                 recordsFilePath = localFileLocation.FileName;
-                labelConsoleOutput.Content = recordsFilePath;
+                labelSelectFile.Content = recordsFilePath;
             }
             
+        }
+
+        private void buttonSelectOutputFolder_Click(object sender, RoutedEventArgs e)
+        {
+            // create a CommonOpenFileDialog object to grab the file path
+            CommonOpenFileDialog outputFolderPath = new CommonOpenFileDialog();
+            outputFolderPath.IsFolderPicker = true;
+            if (outputFolderPath.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                // pass the folder path to outputFolderPath string
+                outputPath = outputFolderPath.FileName + "\\";
+                labelSelectOutput.Content = outputPath;
+            }
         }
     }
 }
